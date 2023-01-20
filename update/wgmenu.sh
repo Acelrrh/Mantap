929ef50dcd3f014c02c5c6a88fa0fb50b99b47a9
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-21 06:54:15 +0700

    Update wgmenu.sh

--- update/wgmenu.sh
+++ update/wgmenu.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-clear
-m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
-yl="\033[0;1;33m"
-wh="\033[0m"
-echo -e "$y                          WIREGUARD $wh"
-echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y. Create Account Wireguard"
-echo -e "$yy 2$y. Delete Account Wireguard"
-echo -e "$yy 3$y. Extending Account Wireguard Active Life"
-echo -e "$yy 4$y. Menu"
-echo -e "$yy 5$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 5 ] : " menu
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m       • WIREGUARD •         \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-case $menu in
+echo -e " [\e[36m•1\e[0m] Create Account WIREGUARD"
+echo -e " [\e[36m•2\e[0m] Delete Account WIREGUARD "
+echo -e " [\e[36m•3\e[0m] Extending Account WIREGUARD Active Life"
+echo -e " [\e[36m•4\e[0m] MENU "
+echo -e " [\e[36m•5\e[0m] KELUAR "
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu :  "  opt
 1)
 addwg
 ;;
