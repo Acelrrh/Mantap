7d27f747c91a3f1fbe014aba20f58c189f977121
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-20 20:42:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -71,7 +71,7 @@ wget https://${akbarvpnnnnnnnnn}/edu.sh && chmod +x edu.sh && ./edu.sh
 # Ohp Server
 wget https://${akbarvpnnnnnnnnnn}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 # Install SlowDNS
-wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
+wget https://raw.githubusercontent.com/Acelrrh/Mantap/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
 # Informasi IP Saya dan Semua Port TCP UDP
  wget https://raw.githubusercontent.com/Acelrrh/Mantap/main/ipsaya.sh && chmod +x ipsaya.sh
 #
