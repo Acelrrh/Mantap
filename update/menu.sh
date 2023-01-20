4e21a38eaa71baa1d4274120ffa69dc72e8721c9
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-20 23:30:14 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -1,141 +1,132 @@
 #!/bin/bash
-clear
-m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
-yl="\033[0;1;33m"
-wh="\033[0m"
-clear
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo "Checking VPS"
+#########################
+IZIN=$(curl -sS https://raw.githubusercontent.com/Acelrrh/supreme/main/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
+#EXPIRED
+expired=$(curl -sS https://raw.githubusercontent.com/Acelrrh/supreme/main/permission/ip | grep $MYIP | awk '{print $3}')
+echo $expired > /root/expired.txt
+today=$(date -d +1day +%Y-%m-%d)
+while read expired
+do
+	exp=$(echo $expired | curl -sS https://raw.githubusercontent.com/Acelrrh/supreme//permission/ip | grep $MYIP | awk '{print $3}')
+	if [[ $exp < $today ]]; then
+		Exp2="\033[1;31mExpired\033[0m"
+        else
+        Exp2=$(curl -sS https://raw.githubusercontent.com/Acelrrh/supreme/main/permission/ip | grep $MYIP | awk '{print $3}')
+	fi
+done < /root/expired.txt
+rm /root/expired.txt
+Name=$(curl -sS https://raw.githubusercontent.com/Acelrrh/supreme/main/permission/ip | grep $MYIP | awk '{print $2}')
+# Color Validation
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+# VPS Information
+#Domain
+domain=$(cat /etc/xray/domain)
+#Status certificate
+modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
+modifyTime1=$(date +%s -d "${modifyTime}")
+currentTime=$(date +%s)
+stampDiff=$(expr ${currentTime} - ${modifyTime1})
+days=$(expr ${stampDiff} / 86400)
+remainingDays=$(expr 90 - ${days})
+tlsStatus=${remainingDays}
+if [[ ${remainingDays} -le 0 ]]; then
+	tlsStatus="expired"
+fi
+# OS Uptime
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+# Download
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
+ISP=$(curl -s ipinfo.io/org?token=7578ac19afd785 | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city?token=7578ac19afd785 )
+WKT=$(curl -s ipinfo.io/timezone?token=7578ac19afd785 )
+DAY=$(date +%A)
+DATE=$(date +%m/%d/%Y)
+DATE2=$(date -R | cut -d " " -f -5)
+IPVPS=$(curl -s ipinfo.io/ip?token=7578ac19afd785 )
+cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+tram=$( free -m | awk 'NR==2 {print $2}' )
+uram=$( free -m | awk 'NR==2 {print $3}' )
+fram=$( free -m | awk 'NR==2 {print $4}' )
+clear 
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e                • SUPREME •                 "
+echo -e "                 • SUPREME •                 "
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[33mOS            \e[0m:  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
-echo -e "\e[33mIP            \e[0m:  $IPVPS"	
-echo -e "\e[33mWATSHAPP      \e[0m:+6281374452477
-echo -e "\e[33mNEGARA        \e[0m:  INDONESIA"
-echo -e "\e[33mDOMAIN        \e[0m:  $domain"	
-echo -e "\e[33mDATE & TIME   \e[0m:  $DATE2"	
+echo -e "\e[33m OS            \e[0m:  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
+echo -e "\e[33m IP            \e[0m:  $IPVPS"	
+echo -e "\e[33m ASN           \e[0m:  $ISP"
+echo -e "\e[33m CITY          \e[0m:  $CITY"
+echo -e "\e[33m DOMAIN        \e[0m:  $domain"	
+echo -e "\e[33m DATE & TIME   \e[0m:  $DATE2"	
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[33m                 • SCRIPT MENU •                 "
-echo -e "\[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\                MENU UTAMA $yy"
-echo -e "\            Script Mod By BABANG BOY $yy"
-echo -e "y-------------------------------------------------$m"
-echo -e "yy 1y.  SSH & OpenVPN MENU  yl"
-echo -e "yy 2y.  L2TP MENU$yl"
-echo -e "yy 3y.  PPTP MENU$yl"
-echo -e "yy 4y.  SSTP MENU$yl"
-echo -e "yy 5y.  WIREGUARD MENUyl"
-echo -e "yy 6y.  SHADOWSOCKS MENUyl"
-echo -e "yy 7y.  SHADOWSOCKSR MENUyl"
-echo -e "yy 8y.  XRAY VMESS MENUyl"
-echo -e "yy 9y.  XRAY VLESS MENUwh"
-echo -e "yy 10y. XRAY TROJAN MENUyl"
-echo -e "yy 11y. TROJAN GO MENUyl"
-echo -e "yy 12y. XRAY GRPC MENUyl"
-echo -e "yy 13y. SLOWDNS MENU yl"
-echo -e "yy 14y. CEK SEMUA IP PORTyl"
-echo -e "yy 15y. CEK SEMUA SERVICE VPNyl"
-echo -e "yy 16 . UPDATE MENU (Update) "
-echo -e "yy 17y. sl-fix (Perbaiki Error SSLH+WS-TLS setelah reboot)yl"
-echo -e "yy 18y. Settings (Pengaturan)$wh"
-echo -e "yy 19y. Exit (Keluar)$wh"
-echo -e "yy 20y. copyrepo (Salin Repo Script Mantap)yl"
-echo -e "yy 21y. menuinfo (Untuk Mendapatkan Informasi)yl"
-echo -e "yy 22y. Shadowsocks Plugin (Buat Akun)wh"
-echo -e "y-------------------------------------------------$m"
-read -p "Select From Options [ 1 - 22 ] : " menu
-case $menu in
-1)
-clear
-sshovpnmenu
-;;
-2)
-clear
-l2tpmenu
-;;
-3)
-clear
-pptpmenu
-;;
-4)
-clear
-sstpmenu
-;;
-5)
-clear
-wgmenu
-;;
-6)
-clear
-ssmenu
-;;
-7)
-clear
-ssrmenu
-;;
-8)
-clear
-vmessmenu
-;;
-9)
-clear
-vlessmenu
-;;
-10)
-clear
-trmenu
-;;
-11)
-clear
-trgomenu
-;;
-12)
-clear
-grpcmenu
-;;
-13)
-clear
-slowdnsmenu
-;;
-14)
-clear
-ipsaya
-;;
-15)
-clear
-running
-;;
-16)
-clear
-updatemenu
-;;
-17)
-clear
-sl-fix
-;;
-18)
-clear
-setmenu
-;;
-19)
-clear
-exit
-;;
-20)
-clear
-copyrepo
-;;
-21)
-clear
-menuinfo
-;;
-22)
-clear
-addss-p
-;;
-*)
-clear
-menu
-;;
+echo -e "                 • SCRIPT MENU •                 "
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " [\e[36m•1\e[0m] SSH Menu"
+echo -e " [\e[36m•2\e[0m] Vmess Menu"
+echo -e " [\e[36m•3\e[0m] Vless Menu"
+echo -e " [\e[36m•4\e[0m] Shadowsocks Menu"
+echo -e " [\e[36m•5\e[0m] Trojan Menu"
+echo -e " [\e[36m•6\e[0m] System Menu"
+echo -e " [\e[36m•7\e[0m] Status Service"
+echo -e " [\e[36m•8\e[0m] Clear RAM Cache"
+echo -e   ""
+echo -e   " Press x or [ Ctrl+C ] • To-Exit-Script"
+echo -e   ""
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[33mClient Name \E[0m: $Name"
+echo -e " \e[33mExpired     \E[0m: $Exp2"
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e   ""
+read -p " Select menu :  "  opt
+echo -e   ""
+case $opt in
+1) clear ; m-sshovpn ;;
+2) clear ; m-vmess ;;
+3) clear ; m-vless ;;
+4) clear ; m-ssws ;;
+5) clear ; m-trojan ;;
+6) clear ; m-system ;;
+7) clear ; running ;;
+8) clear ; clearcache ;;
+x) exit ;;
+*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
