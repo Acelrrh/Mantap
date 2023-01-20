478e29a70562e21bef0a7d76ff77c771932d0e3f
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-21 06:39:50 +0700

    Update sstpmenu.sh

--- update/sstpmenu.sh
+++ update/sstpmenu.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-clear
-m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
-yl="\033[0;1;33m"
-wh="\033[0m"
-echo -e "$y                                SSTP $wh"
-echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y. Create Account SSTP"
-echo -e "$yy 2$y. Delete Account SSTP"
-echo -e "$yy 3$y. Extending Account SSTP Active Life"
-echo -e "$yy 4$y. Check User Login SSTP"
-echo -e "$yy 5$y. Menu"
-echo -e "$yy 6$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 6 ] : " menu
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m       • SSTP•         \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-case $menu in
+echo -e " [\e[36m•1\e[0m] Create Account SSTP
+echo -e " [\e[36m•2\e[0m] Delete Account SSTPP "
+echo -e " [\e[36m•3\e[0m] Extending Account SSTP Active Life"
+echo -e " [\e[36m•4\e[0m] Check User Login SSTP "
+echo -e " [\e[36m•4\e[0m] MENU "
+echo -e " [\e[36m•5\e[0m] KELUAR "
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu :  "  opt
 1)
 addsstp
 ;;
