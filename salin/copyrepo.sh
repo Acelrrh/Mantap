2d074de59dec50d5c3018bf5c71eb6577d3685fb
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-21 04:24:55 +0700

    Update copyrepo.sh

--- salin/copyrepo.sh
+++ salin/copyrepo.sh
@@ -1,12 +1,39 @@
-#!/bin/bash
+syarat ketentuan berlangganan
 
-cd
-sudo apt update -y
-apt update -y
-sudo apt install git -y
-apt install git -y
-cd
-cd /root/
-git clone https://github.com/fisabiliyusri/Mantap
+1. 1IP 25.000. Untuk setup sendiri bebas kapan pun anda mau
+2. 10.000. kami bantu setup kan..
+3. jika terjadi eror karna kesalan sediri pengguna hendak meminta install ulang harus sabar(jika menggunakan jasa setup yg no 2)
+4. Jika sudah di order tidak bisa di readfun kembali dengan alasan apa pun.
 
-cd
+Cara ingin berlangganan :
+kirim dana  anda ke  payment OVO :081374452477
+Setelah di kirim secrinshoot bukti transfer tersebut berikut dengan vps dan paswoord(jika memilih no 2)hanya jasa setup) atau kirim ip vps anda tanpa paswoord jika memilih nomor 1(install sendiri)
+
+
+informasi tentang sistem kami...
+
+Printah mengganti banner
+/etc/issue.net" 
+
+==============================
+============================================================
+printah mengganti port ws non tls 
+
+/etc/systemd/system/ws-nontls.service 
+
+============================================================
+Printah cex/ganti port ws opvn
+/etc/systemd/system/ws-ovpn.service 
+
+============================================================printah mengganti port ws tls
+
+etc/systemd/system/ws-tls.service
+
+============================================================
+printa untuk mengganti ws.tls
+etc/systemd/system/ws-tls.service
+
+untukinfo ini yg bisa kami sampai
+untuk bb
+
+
