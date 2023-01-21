851726ae1446bbe7d77eb5b3866a96d97c2dd4fd
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-21 07:42:48 +0700

    Update and rename copyrepo.txt to copyrepo.sh

---
+++ salin/copyrepo.sh
@@ -0,0 +1,40 @@
+echo -e " ecsyarat ketentuan berlangganan"
+
+echo -e ". 1IP 25.000. Untuk setup sendiri bebas kapan pun anda mau "  
+echo -e "   2. 10.000. kami bantu setup kan.."
+echo -e "  3. jika terjadi eror karna kesalan sediri pengguna hendak meminta install ulang harus sabar(jika menggunakan jasa setup yg no 2)"
+echo -e "  4. Jika sudah di order tidak bisa di readfun kembali dengan alasan apa pun."
+
+echo -e " Cara ingin berlangganan :"
+echo -e " kirim dana  anda ke  payment OVO :081374452477"
+echo -e " Setelah di kirim secrinshoot bukti transfer tersebut berikut dengan vps dan paswoord(jika memilih no 2)hanya jasa setup) atau kirim ip vps anda tanpa paswoord jika memilih nomor 1(install sendiri)"
+
+
+echo -e " informasi tentang sistem kami..."
+
+echo -e " Printah mengganti banner"
+echo -e " /etc/issue.net" 
+
+echo -e "=============================="
+echo -e "============================================================"
+echo -e " printah mengganti port ws non tls"
+
+echo -e " /etc/systemd/system/ws-nontls.service" 
+
+============================================================"
+echo -e " Printah cex/ganti port ws opvn"
+echo -e " /etc/systemd/system/ws-ovpn.service"
+
+echo -e " ============================================================"
+echo -e "     printah mengganti port ws tls"
+
+echo -e " etc/systemd/system/ws-tls.service"
+
+echo -e " ============================================================"
+echo -e " printa untuk mengganti ws.tls
+echo -e " etc/systemd/system/ws-tls.service
+
+echo -e " untukinfo ini yg bisa kami sampai"
+echo -e "sampaikan"
+
+
