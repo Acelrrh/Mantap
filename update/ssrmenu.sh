1e3f870993b4285162252b3c02c9fb6c76b74293
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-21 07:17:26 +0700

    Update ssrmenu.sh

--- update/ssrmenu.sh
+++ update/ssrmenu.sh
@@ -8,8 +8,7 @@ echo -e " [\e[36m•1\e[0m] Create Account SSR"
 echo -e " [\e[36m•2\e[0m] Delete Account SSR "
 echo -e " [\e[36m•3\e[0m] Extending Account SSR Active Life"
 echo -e " [\e[36m•4\e[0m] Show Other SSR Menu "
-echo -e " [\e[36m•5\e[0m] MENU "
-echo -e " [\e[36m•6\e[0m] KELUAR "
+echo -e " [\e[36m•5\e[0m] KELUAR "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -29,10 +28,6 @@ renewssr
 ;;
 4)
 ssr
-;;
-5)
-clear
-menu
 ;;
 6)
 clear
