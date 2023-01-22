b729e0ba2ed96f46fd1b6184084b61e521fd9973
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-22 13:34:09 +0700

    Update install-ss-plugin.sh

--- shadowsocks-plugin/install-ss-plugin.sh
+++ shadowsocks-plugin/install-ss-plugin.sh
@@ -21,7 +21,7 @@ wget -O addss-p "https://raw.githubusercontent.com/Acelrrh/Mantap/main/shadowsoc
 chmod +x addss-p
 wget -O shadowhost "https://raw.githubusercontent.com/Acelrrh/Mantap/main/shadowsocks-plugin/shadowhost.sh"
 chmod +x shadowhost && shadowhost
-wget -O delss "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/shadowsocks/delss.sh"
+wget -O delss "https://raw.githubusercontent.com/Acelrrh/Mantap/main/shadowsocks/delss.sh"
 cd
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
