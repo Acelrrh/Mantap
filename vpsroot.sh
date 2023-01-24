ffb5357fc3ea511a4331122ae803653ae8d9c06d
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-25 00:11:13 +0700

    Update vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -2,5 +2,5 @@
 # Mod By SL
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
 wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/acelrrh/Mantap/main/sshd_config
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/acelrrh/Mantap/main/slhost.sh
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/acelrrh/Mantap/main/slhost
 systemctl restart sshd;
