dbeed22950d9bdfe77eaf7e6fe4d41c2a91287fb
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-25 00:31:57 +0700

    Update vpnroot.sh

--- vpnroot.sh
+++ vpnroot.sh
@@ -2,5 +2,5 @@
 # Mod By SL
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
 wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/acelrrh/Mantap/main/sshd_config
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/acelrrh/Mantap/main/slhost
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/acelrrh/Mantap/main/addhost.sh
 systemctl restart sshd;
