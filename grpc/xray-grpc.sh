8f82d207315526b0fe6f3bb02b36e7ba641e7b6e
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-24 15:20:39 +0700

    Update xray-grpc.sh

--- grpc/xray-grpc.sh
+++ grpc/xray-grpc.sh
@@ -319,10 +319,10 @@ systemctl enable vless-grpc
 systemctl restart vless-grpc
 #
 cd /usr/bin
-wget -O addgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/addgrpc.sh"
-wget -O delgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/delgrpc.sh"
-wget -O renewgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/renewgrpc.sh"
-wget -O cekgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/cekgrpc.sh"
+wget -O addgrpc "https://raw.githubusercontent.com/acelrrh/Mantap/main/grpc/addgrpc.sh"
+wget -O delgrpc "https://raw.githubusercontent.com/acelrrh/Mantap/main/grpc/delgrpc.sh"
+wget -O renewgrpc "https://raw.githubusercontent.com/acelrrh/Mantap/main/grpc/renewgrpc.sh"
+wget -O cekgrpc "https://raw.githubusercontent.com/acelrrh/Mantap/main/grpc/cekgrpc.sh"
 
 chmod +x addgrpc
 chmod +x delgrpc
