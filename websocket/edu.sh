b09ff91828112a859dc1658e6b97f8c719159842
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-24 14:56:51 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -25,6 +25,8 @@ AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 80
 Restart=on-failure
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 143
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
