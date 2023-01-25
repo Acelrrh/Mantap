16f952338dbcd0122cc7c1ccde80371288e5b111
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-25 11:25:33 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -1,1 +1,139 @@
 IyEvYmluL2Jhc2gKY2xlYXIKbT0iXDAzM1swOzE7MzZtIgp5PSJcMDMzWzA7MTszN20iCnl5PSJcMDMzWzA7MTszMm0iCnlsPSJcMDMzWzA7MTszM20iCndoPSJcMDMzWzBtIgplY2hvIC1lICIkeSAgICAgICAgICAgICAgICAgIE1FTlUgVVRBTUEgJHdoIgplY2hvIC1lICIkeSAgICAgICAgICAgIFNjcmlwdCBNb2QgQnkgU0wgJHdoIgplY2hvIC1lICIkeS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0kd2giCmVjaG8gLWUgIiR5eSAxJHkuICBTU0ggJiBPcGVuVlBOIE1FTlUgICR3aCIKZWNobyAtZSAiJHl5IDIkeS4gIEwyVFAgTUVOVSR3aCIKZWNobyAtZSAiJHl5IDMkeS4gIFBQVFAgTUVOVSR3aCIKZWNobyAtZSAiJHl5IDQkeS4gIFNTVFAgTUVOVSR3aCIKZWNobyAtZSAiJHl5IDUkeS4gIFdJUkVHVUFSRCBNRU5VJHdoIgplY2hvIC1lICIkeXkgNiR5LiAgU0hBRE9XU09DS1MgTUVOVSR3aCIKZWNobyAtZSAiJHl5IDckeS4gIFNIQURPV1NPQ0tTUiBNRU5VJHdoIgplY2hvIC1lICIkeXkgOCR5LiAgWFJBWSBWTUVTUyBNRU5VJHdoIgplY2hvIC1lICIkeXkgOSR5LiAgWFJBWSBWTEVTUyBNRU5VJHdoIgplY2hvIC1lICIkeXkgMTAkeS4gWFJBWSBUUk9KQU4gTUVOVSR3aCIKZWNobyAtZSAiJHl5IDExJHkuIFRST0pBTiBHTyBNRU5VJHdoIgplY2hvIC1lICIkeXkgMTIkeS4gWFJBWSBHUlBDIE1FTlUkd2giCmVjaG8gLWUgIiR5eSAxMyR5LiBTTE9XRE5TIE1FTlUgKE9GRikkd2giCmVjaG8gLWUgIiR5eSAxNCR5LiBDRUsgU0VNVUEgSVAgUE9SVCR3aCIKZWNobyAtZSAiJHl5IDE1JHkuIENFSyBTRU1VQSBTRVJWSUNFIFZQTiR3aCIKZWNobyAtZSAiJHl5IDE2JHkuIFVQREFURSBNRU5VIChVcGRhdGUpJHdoIgplY2hvIC1lICIkeXkgMTckeS4gc2wtZml4IChQZXJiYWlraSBFcnJvciBTU0xIK1dTLVRMUyBzZXRlbGFoIHJlYm9vdCkkd2giCmVjaG8gLWUgIiR5eSAxOCR5LiBTZXR0aW5ncyAoUGVuZ2F0dXJhbikkd2giCmVjaG8gLWUgIiR5eSAxOSR5LiBFeGl0IChLZWx1YXIpJHdoIgplY2hvIC1lICIkeXkgMjAkeS4gY29weXJlcG8gKFNhbGluIFJlcG8gU2NyaXB0IE1hbnRhcCkkd2giCmVjaG8gLWUgIiR5eSAyMSR5LiBtZW51aW5mbyAoVW50dWsgTWVuZGFwYXRrYW4gSW5mb3JtYXNpKSR3aCIKZWNobyAtZSAiJHl5IDIyJHkuIFNoYWRvd3NvY2tzIFBsdWdpbiAoQnVhdCBBa3VuKSR3aCIKZWNobyAtZSAiJHktLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tJHdoIgpyZWFkIC1wICJTZWxlY3QgRnJvbSBPcHRpb25zIFsgMSAtIDIyIF0gOiAiIG1lbnUKY2FzZSAkbWVudSBpbgoxKQpjbGVhcgpzc2hvdnBubWVudQo7OwoyKQpjbGVhcgpsMnRwbWVudQo7OwozKQpjbGVhcgpwcHRwbWVudQo7Owo0KQpjbGVhcgpzc3RwbWVudQo7Owo1KQpjbGVhcgp3Z21lbnUKOzsKNikKY2xlYXIKc3NtZW51Cjs7CjcpCmNsZWFyCnNzcm1lbnUKOzsKOCkKY2xlYXIKdm1lc3NtZW51Cjs7CjkpCmNsZWFyCnZsZXNzbWVudQo7OwoxMCkKY2xlYXIKdHJtZW51Cjs7CjExKQpjbGVhcgp0cmdvbWVudQo7OwoxMikKY2xlYXIKZ3JwY21lbnUKOzsKMTMpCmNsZWFyCnNsb3dkbnNtZW51Cjs7CjE0KQpjbGVhcgppcHNheWEKOzsKMTUpCmNsZWFyCnJ1bm5pbmcKOzsKMTYpCmNsZWFyCnVwZGF0ZW1lbnUKOzsKMTcpCmNsZWFyCnNsLWZpeAo7OwoxOCkKY2xlYXIKc2V0bWVudQo7OwoxOSkKY2xlYXIKZXhpdAo7OwoyMCkKY2xlYXIKY29weXJlcG8KOzsKMjEpCmNsZWFyCm1lbnVpbmZvCjs7CjIyKQpjbGVhcgphZGRzcy1wCjs7CiopCmNsZWFyCm1lbnUKOzsKZXNhYwo=
+#!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "                 • SUPREME •                 "
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[33m OS            \e[0m:  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
+echo -e "\e[33m IP            \e[0m:  $IPVPS"	
+echo -e "\e[33m ASN           \e[0m:  $ISP"
+echo -e "\e[33m WA          \e[0m:  +6281374452477"
+echo -e "\e[33m DOMAIN        \e[0m:  $domain"	
+echo -e "\e[33m DATE & TIME   \e[0m:  $DATE2"	
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "                 • SCRIPT MENU •                 "
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$y-------------------------------------------------$wh"
+echo -e "$yy 1$y.  SSH & OpenVPN MENU  $wh"
+echo -e "$yy 2$y.  L2TP MENU$wh"
+echo -e "$yy 3$y.  PPTP MENU$wh"
+echo -e "$yy 4$y.  SSTP MENU$wh"
+echo -e "$yy 5$y.  WIREGUARD MENU$wh"
+echo -e "$yy 6$y.  SHADOWSOCKS MENU$wh"
+echo -e "$yy 7$y.  SHADOWSOCKSR MENU$wh"
+echo -e "$yy 8$y.  XRAY VMESS MENU$wh"
+echo -e "$yy 9$y.  XRAY VLESS MENU$wh"
+echo -e "$yy 10$y. XRAY TROJAN MENU$wh"
+echo -e "$yy 11$y. TROJAN GO MENU$wh"
+echo -e "$yy 12$y. XRAY GRPC MENU$wh"
+echo -e "$yy 13$y. SLOWDNS MENU (OFF)$wh"
+echo -e "$yy 14$y. CEK SEMUA IP PORT$wh"
+echo -e "$yy 15$y. CEK SEMUA SERVICE VPN$wh"
+echo -e "$yy 16$y. UPDATE MENU (Update)$wh"
+echo -e "$yy 17$y. sl-fix (Perbaiki Error SSLH+WS-TLS setelah reboot)$wh"
+echo -e "$yy 18$y. Settings (Pengaturan)$wh"
+echo -e "$yy 19$y. Exit (Keluar)$wh"
+echo -e "$yy 20$y. copyrepo (Salin Repo Script Mantap)$wh"
+echo -e "$yy 21$y. menuinfo (Untuk Mendapatkan Informasi)$wh"
+echo -e "$yy 22$y. Shadowsocks Plugin (Buat Akun)$wh"
+echo -e "$y-------------------------------------------------$wh"
+read -p "Select From Options [ 1 - 22 ] : " menu
+case $menu in
+1)
+clear
+sshovpnmenu
+;;
+2)
+clear
+l2tpmenu
+;;
+3)
+clear
+pptpmenu
+;;
+4)
+clear
+sstpmenu
+;;
+5)
+clear
+wgmenu
+;;
+6)
+clear
+ssmenu
+;;
+7)
+clear
+ssrmenu
+;;
+8)
+clear
+vmessmenu
+;;
+9)
+clear
+vlessmenu
+;;
+10)
+clear
+trmenu
+;;
+11)
+clear
+trgomenu
+;;
+12)
+clear
+grpcmenu
+;;
+13)
+clear
+slowdnsmenu
+;;
+14)
+clear
+ipsaya
+;;
+15)
+clear
+running
+;;
+16)
+clear
+updatemenu
+;;
+17)
+clear
+sl-fix
+;;
+18)
+clear
+setmenu
+;;
+19)
+clear
+exit
+;;
+20)
+clear
+copyrepo
+;;
+21)
+clear
+menuinfo
+;;
+22)
+clear
+addss-p
+;;
+*)
+clear
+menu
+;;
+esac
