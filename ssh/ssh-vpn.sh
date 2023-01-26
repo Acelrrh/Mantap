cdeba625f2db88b34ffe90419e7347a3c51a50f1
Acelrrh <122144317+Acelrrh@users.noreply.github.com>
2023-01-26 09:42:11 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -243,39 +243,39 @@ rm -rf /root/vnstat-2.6
 
 # install stunnel 5 
 cd /root/
-wget -q -O stunnel5.zip "https://${akbarvpnnnn}/stunnel5.zip"
-unzip -o stunnel5.zip
-cd /root/stunnel
-chmod +x configure
-./configure
-make
-make install
-cd /root
-rm -r -f stunnel
-rm -f stunnel5.zip
-mkdir -p /etc/stunnel5
-chmod 644 /etc/stunnel5
+#wget -q -O stunnel5.zip "https://${akbarvpnnnn}/stunnel5.zip"
+#unzip -o stunnel5.zip
+#cd /root/stunnel
+#chmod +x configure
+#./configure
+#make
+#make install
+#cd /root
+#rm -r -f stunnel
+#rm -f stunnel5.zip
+#mkdir -p /etc/stunnel5
+#chmod 644 /etc/stunnel5
 
 # Download Config Stunnel5
-cat > /etc/stunnel5/stunnel5.conf <<-END
-cert = /etc/xray/xray.crt
-key = /etc/xray/xray.key
-client = no
-socket = a:SO_REUSEADDR=1
-socket = l:TCP_NODELAY=1
-socket = r:TCP_NODELAY=1
+#cat > /etc/stunnel5/stunnel5.conf <<-END
+#cert = /etc/xray/xray.crt
+#key = /etc/xray/xray.key
+#client = no
+#socket = a:SO_REUSEADDR=1
+#socket = l:TCP_NODELAY=1
+#socket = r:TCP_NODELAY=1
 
-[dropbear]
-accept = 445
-connect = 127.0.0.1:109
+#[dropbear]
+#accept = 445
+#connect = 127.0.0.1:109
 
-[openssh]
-accept = 777
-connect = 127.0.0.1:443
+#[openssh]
+#accept = 777
+#connect = 127.0.0.1:443
 
-[openvpn]
-accept = 990
-connect = 127.0.0.1:1194
+#[openvpn]
+#accept = 990
+#connect = 127.0.0.1:1194
 
 
 END
@@ -287,7 +287,7 @@ END
 #cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
 
 # Service Stunnel5 systemctl restart stunnel5
-cat > /etc/systemd/system/stunnel5.service << END
+#cat > /etc/systemd/system/stunnel5.service << END
 [Unit]
 Description=Stunnel5 Service
 Documentation=https://stunnel.org
@@ -303,29 +303,29 @@ WantedBy=multi-user.target
 END
 
 # Service Stunnel5 /etc/init.d/stunnel5
-wget -q -O /etc/init.d/stunnel5 "https://${akbarvpnnnn}/stunnel5.init"
+#wget -q -O /etc/init.d/stunnel5 "https://${akbarvpnnnn}/stunnel5.init"
 
 # Ubah Izin Akses
-chmod 600 /etc/stunnel5/stunnel5.pem
-chmod +x /etc/init.d/stunnel5
+#chmod 600 /etc/stunnel5/stunnel5.pem
+#chmod +x /etc/init.d/stunnel5
 cp /usr/local/bin/stunnel /usr/local/bin/stunnel5
 
 # Remove File
-rm -r -f /usr/local/share/doc/stunnel/
-rm -r -f /usr/local/etc/stunnel/
-rm -f /usr/local/bin/stunnel
-rm -f /usr/local/bin/stunnel3
-rm -f /usr/local/bin/stunnel4
+#rm -r -f /usr/local/share/doc/stunnel/
+#rm -r -f /usr/local/etc/stunnel/
+#rm -f /usr/local/bin/stunnel
+#rm -f /usr/local/bin/stunnel3
+#rm -f /usr/local/bin/stunnel4
 #rm -f /usr/local/bin/stunnel5
 
 # Restart Stunnel 5
-systemctl stop stunnel5
-systemctl enable stunnel5
-systemctl start stunnel5
-systemctl restart stunnel5
-/etc/init.d/stunnel5 restart
-/etc/init.d/stunnel5 status
-/etc/init.d/stunnel5 restart
+#systemctl stop stunnel5
+#systemctl enable stunnel5
+#systemctl start stunnel5
+#systemctl restart stunnel5
+#/etc/init.d/stunnel5 restart
+#/etc/init.d/stunnel5 status
+#/etc/init.d/stunnel5 restart
 
 #OpenVPN
 wget https://${akbarvpn}/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
